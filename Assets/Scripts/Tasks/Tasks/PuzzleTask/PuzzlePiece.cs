dc6a8dc885c0f2c8461f5beeee328304b83e3dd0
Jakub Michalski <robertmenel@gmail.com>
2023-03-04 17:45:14 +0100

    Added dotween finished puzzle fithout vr interaction

--- Assets/Scripts/Tasks/Tasks/PuzzleTask/PuzzlePiece.cs
+++ Assets/Scripts/Tasks/Tasks/PuzzleTask/PuzzlePiece.cs
@@ -1,19 +1,26 @@
 ﻿using UnityEngine;
 using NaughtyAttributes;
-
+using DG.Tweening;
 public class PuzzlePiece : MonoBehaviour
 {
     [SerializeField] private PuzzleTypeDeterminator puzzleTypeDeterminator = null;
     private PuzzlePieceType currentPuzzlePieceType = PuzzlePieceType.Triangle;
-    private int currentPuzzlePieceIndex = 0;
-
+    [SerializeField,ReadOnly]private int currentPuzzlePieceIndex = 0;
+    
+    private void Start()
+    {
+        //randomize the starting position but different than the correct position
+        currentPuzzlePieceIndex = Random.Range(0, 4);
+        rotateObjectBasedOnIndex(currentPuzzlePieceIndex);
+        currentPuzzlePieceType = (PuzzlePieceType)currentPuzzlePieceIndex;
+    }
 
     public bool IsInCorrectPosition()
     {
         return currentPuzzlePieceType == puzzleTypeDeterminator.PuzzlePieceType;
     }
 
-    public void OnInteraction()
+    public void OnInteraction() // used by XRInteractionComponent
     {
         currentPuzzlePieceIndex++;
 
@@ -24,9 +31,14 @@ public class PuzzlePiece : MonoBehaviour
 
         currentPuzzlePieceType = (PuzzlePieceType)currentPuzzlePieceIndex;
 
-        gameObject.transform.rotation = Quaternion.Euler(0, currentPuzzlePieceIndex * 90, 0);
+        rotateObjectBasedOnIndex(currentPuzzlePieceIndex);
     }
 
+    private void rotateObjectBasedOnIndex(int index)
+    {
+        gameObject.transform.DOLocalRotate(new Vector3(0, index * 90, 0), 0.1f, RotateMode.Fast);
+    }
+    
 #if UNITY_EDITOR
     [Button]
     private void TestInteraction()
